7e7fa8c785a851158ae7dd2045854c39d2f82c37
Vitor Mateus <vmateusaguiar@gmail.com>
2022-06-07 09:38:19 -0300

    curso ignorância zero - aula 23

---
+++ python/curso-ignorancia-zero/023-aula/area-circulo.py
@@ -0,0 +1,10 @@
+#import math
+#import math as m
+#from math import pi as p
+from math import pi
+
+raio = float(input('Digite o tamanho do raio: '))
+
+area = pi * raio**2
+
+print(f'A área do círculo é {area}')
